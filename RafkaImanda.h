f60a4477e1c27d35b008322ba7c75abc87324c57
Raff <rafka.0312@gmail.com>
2024-05-13 09:33:18 +0700

    Add: Header for Tree Encryption

--- RafkaImanda.h
+++ RafkaImanda.h
@@ -14,6 +14,14 @@ typedef struct{
 	char kelas[KELAS_MAX];
 }db_mahasiswa[N_MAX];
 
+typedef struct Node {
+    char data;
+    int key;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Autentikasi
 void userAuth();
 int authMenu();
 void inputCredentials(char *username[CHAR_MAX_SIZE], char *password[CHAR_MAX_SIZE]);
@@ -29,4 +37,12 @@ void redirectMhsMenu(int pilihanMenu);
 void enkripsiVernam(char* plain_text, char* key, char *cipher_text);
 void decryptVernam(char* cipher_text, char* key, char* decryptedMsg);
 char* encryptPassword(char *password);
+
+// Enkripsi dengan Tree
+Node* newNode(char data, int key);
+Node* insert(Node* node, char data, int key);
+char* inorder(struct Node* temp);
+void chiper(char* password, int len);
+char* encryptPassword(char *password);
+
 #endif // RafkaImanda
