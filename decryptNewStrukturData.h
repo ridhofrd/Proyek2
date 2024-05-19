6a43c8ef4a3c0df1330baae7d92517b7e48b8a8a
Annisa Dian Fadillah <96567875+AnnisaDianFadillah06@users.noreply.github.com>
2024-05-19 13:42:24 +0700

    Update decryptNewStrukturData.h
    Pakai union

--- decryptNewStrukturData.h
+++ decryptNewStrukturData.h
@@ -10,33 +10,61 @@
 
 // Define constants
 #define MAX 100
-typedef struct Message {
+
+// Enum for encryption methods
+typedef enum {
+    RAILFENCE = 1,
+    HILL = 2,
+    VERNAM = 3,
+    VIGENERE = 4,
+    RSA = 5
+} CipherType;
+
+// Struct for linked list messages
+typedef struct MessageNode {
     int status;
-    int id;
-    char timestamp[100];
+    int id_pesan;
+    char timestamp[50];
     char nama_target[50];
-    char nama_pengirim[100];
-    int method;
-    int n; // Ukuran matriks untuk metode 2
-    char key[100];
-    char encryptedMsg[100];
-    struct Message *next;
-} Message;
+    char nama_pengirimpesan[50];
+    char ciphertext[50];
+    CipherType type;
+    union {
+        struct {
+            int keyangka;
+        } railfence;
+        struct {
+            int n2;
+            char keyhuruf[50];
+        } hill;
+        struct {
+            char keyhuruf[50];
+        } vernam;
+        struct {
+            char keyhuruf[50];
+        } vigenere;
+        struct {
+            int p;
+            int q;
+        } rsa;
+    } cipher;
+    struct MessageNode *next;
+} MessageNode;
 
 // Declare functions
 void decryptRailFence(char encryptedMsg[], int key, char decryptedMsg[]);
 void decryptVigenere(const char *encryptedMsg, const char *key, char *decryptedMsg);
 void decryptVernam(char* cipher_text, char* key, char* decryptedMsg);
 void displayDecryptedMessage(const char *decryptedMsg);
-void viewDecryptionKeyAndMessage(char nama_pengirimpesan[], Message *head, const char *filename);
-
+void viewDecryptionKeyAndMessage(char nama_pengirimpesan[], MessageNode *head, const char *filename);
+void decryptMessage(int key[MAX][MAX], int inverse[MAX][MAX], char cipher_text[], char decryptedMsg[], int n);
 void clearScreen();
 int countLinesInFile(const char *filename);
-Message* createMessage(int status, int id, const char *timestamp, const char *nama_target, const char *nama_pengirim, int method, const char *key, int n, const char *encryptedMsg);
-Message* loadMessagesFromFile(const char *filename);
-int countUnreadMessages(Message *head);
-void markMessageAsRead(Message *head, int messageId);
-void saveMessagesToFile(const char *filename, Message *head);
-void viewDecryptionKeyAndMessageUnRead(char nama_pengirimpesan[], Message *head, const char *filename);
+MessageNode* createMessageNode(int status, int id, const char *timestamp, const char *nama_target, const char *nama_pengirim, CipherType type, const char *key1, const char *key2, const char *ciphertext);
+MessageNode* loadMessagesFromFile(const char *filename);
+int countUnreadMessages(MessageNode *head);
+void markMessageAsRead(MessageNode *head, int messageId);
+void saveMessagesToFile(const char *filename, MessageNode *head);
+void viewDecryptionKeyAndMessageUnRead(char nama_pengirimpesan[], MessageNode *head, const char *filename);
 
 #endif
